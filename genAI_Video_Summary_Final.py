e546553dcb7b5d443b6f934c40133dfd56aee602
vasu-2003-vl <vasanthalakshmiv917@gmail.com>
2025-04-23 12:10:28 +0530

    Update genAI_Video_Summary_Final.py

--- genAI_Video_Summary_Final.py
+++ genAI_Video_Summary_Final.py
@@ -79,7 +79,7 @@ def turn_into_story(summary):
   return model.invoke(prompt)
 
 st.title("AjithAI - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("AjithAI_Transperent")
+st.image("AjithAI_Transperent.jpg")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
